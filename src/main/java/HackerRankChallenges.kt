08981d71a9b34eed061b27ddebd936a84bb0bf37
cmiram <nyyankeeschris@gmail.com>
2019-12-31 20:45:05 -0500

    merged laptop and desktop files

--- src/main/java/HackerRankChallenges.kt
+++ src/main/java/HackerRankChallenges.kt
@@ -1,6 +1,8 @@
 package main.java
 
+import java.util.*
 import kotlin.math.abs
+import kotlin.math.min
 
 fun arrayLeftRotation(a: Array<Int>, d: Int): String {
     return (d until d+a.size).map { a[it%a.size] }.joinToString(" ")
@@ -35,4 +37,55 @@ fun saveThePrisoner(n: Int, m: Int, s: Int): Int {
     val sweets = m % n
     val posToWarn = if (sweets == 0 && s == 1) return n else s + sweets - 1
     return if (posToWarn > n) posToWarn - n else posToWarn
+}
+
+// Complete the breakingRecords function below.
+fun breakingRecords(scores: Array<Int>): Array<Int> {
+    class MutPair<T,V>(var first: T, var second: V)
+    
+    if (scores.size < 2) return arrayOf(0, 0)
+    // track min/max as pair of (value, newRecordCount)
+    val min = MutPair(scores[0], 0)
+    val max = MutPair(scores[0], 0)
+    for (score in scores) {
+        if (score < min.first) {
+            min.first = score
+            min.second += 1
+        } else if (score > max.first) {
+            max.first = score
+            max.second += 1
+        }
+    }
+    return arrayOf(max.second, min.second)
+}
+
+// Complete the birthday function below.
+fun birthday(s: Array<Int>, d: Int, m: Int): Int {
+    return (0..s.size-m).asSequence()
+            .filter { s.sliceArray(IntRange(it, it+m-1)).sum() == d }
+            .count()
+}
+
+// Complete the dayOfProgrammer function below.
+fun dayOfProgrammer(year: Int): String {
+    val calendar = GregorianCalendar()
+    calendar.gregorianChange = Date(Calendar.Builder().setDate(1918, 2, 14).build().timeInMillis)
+    calendar.set(Calendar.YEAR, year)
+    calendar.set(Calendar.DAY_OF_YEAR, 256)
+    val format = "%02d.%02d.%04d"
+    return String.format(format, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.YEAR))
+}
+
+/*
+ * Complete the pageCount function below.
+ */
+fun pageCount(n: Int, p: Int): Int {
+    return if(n-p == 1 && n%2 == 0 && n != 2) 1 else min(p/2, (n-p)/2)
+}
+
+/*
+ * Complete the pageCount function below.
+ */
+fun pageCount1(n: Int, p: Int): Int {
+    return if(n == p || p == 1) 0 else n%2 - n/2
 }
